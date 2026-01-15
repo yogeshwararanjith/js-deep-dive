023ba8b32c806452e9fe4a27bd2f5a1c134ff202
yogeshwararanjith <ranjithyogeshwara0@gmail.com>
2026-01-15 23:37:07 +0530

    time scheduling using setTimeout and setInterval

---
+++ core/Advance working with Functions/recursion/timeScheduling.js
@@ -0,0 +1,35 @@
+// implementing setInterval with setTimeout
+
+// let timerId = setTimeout(function tick(){
+//     console.log('tick')
+//     timerId = setTimeout(tick,1000)
+// },1000)
+
+// output every second
+
+function printNumbers(from, to){
+    let count = from;
+    let timerId = setTimeout(function printNums(){
+        console.log(count)
+        if(count < to){
+            count++;
+            timerId = setTimeout(printNums,1000)
+        }
+    },1000)
+}
+
+printNumbers(1,5)
+
+// using setInterval
+
+function printNumbersWithInterval(from, to){
+    let count = from;
+
+    let timerId = setInterval(function printNums(){
+        console.log(count)
+        if(count === to){
+            clearInterval(timerId)
+        }
+        count++
+    },1000)
+}
