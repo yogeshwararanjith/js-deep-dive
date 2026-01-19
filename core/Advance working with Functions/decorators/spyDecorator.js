830cfe8fd45d0eb0c2495923fd73e0bf95c48caf
yogeshwararanjith <ranjithyogeshwara0@gmail.com>
2026-01-19 18:03:17 +0530

    decorator practice problem

---
+++ core/Advance working with Functions/decorators/spyDecorator.js
@@ -0,0 +1,24 @@
+function spy(func){
+    let calls = [];
+    function wrapper(...args){
+        calls.push(args);
+        let result = func.apply(this,args)
+        return result
+    }
+    wrapper.calls = calls;
+    return wrapper;
+}
+
+function work(a, b) {
+  console.log( a + b ); // work is an arbitrary function or method
+}
+
+
+work = spy(work)
+console.log(work)
+console.log(work(1,2))
+console.log(work(2,2))
+
+for (let args of work.calls) {
+  console.log( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+}
