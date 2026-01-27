2d83546fc0db25255dd84285b04a37cf66f831ac
yogeshwararanjith <ranjithyogeshwara0@gmail.com>
2026-01-27 17:40:02 +0530

    retry and fail with promise

--- Async JS/promise.js
+++ Async JS/promise.js
@@ -24,3 +24,24 @@ function divide(a,b){
 divide(10,2).then((result)=>console.log(result)).catch((error)=>console.log(error));
 divide(10,0).then((result)=>console.log(result)).catch((error)=>console.log(error));
 
+
+
+// retry 3 times then fail, if succeed stop
+
+function unreliableFetch() {
+  return Math.random() > 0.7
+    ? Promise.resolve("success")
+    : Promise.reject("fail");
+}
+
+function retry(attempts){
+    return unreliableFetch().catch(err => {
+        if(attempts === 0){
+            throw new Error('error');
+        }
+        return retry(attempts-1);
+    })
+}
+
+retry(3).then(console.log).catch(console.error)
+
